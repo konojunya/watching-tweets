b1a9777b801489e2776a1213f1dbd33dae5dcfba
konojunya <junya03dance@gmail.com>
2017-06-10 16:36:17 +0900

    無理やりgoroutineを使って複数のアカウントのストリームをつくり、channelでwait

--- main.go
+++ main.go
@@ -8,58 +8,62 @@ import (
 	"regexp"
 )
 
+type Tweet struct {
+	id string
+	text string
+}
+
 func main() {
 
 	api := getTwitterApi()
 
 	var ids map[string]string = make(map[string]string)
 	ids["ms_rinna"] = "3274075003"
-	ids["kinokoruumu416"] = "1662875580"
+	ids["kawamina_happy"] = "726052257121214468"
 
-	streamTweet(api,ids)
+	ch := make(chan string)
+	for id := range ids {
+		go streamTweet(api,id,ids[id],ch)
+	}
+	fmt.Println(<-ch)
 }
 
-func fileExport(filename string, output string) {
+func fileExport(path,text string) {
 
-	file, err := Create("tweets/" + filename + ".txt")
+	file, err := Create("tweets/" + path + ".txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
-	file.Write(([]byte)(output))
+	file.Write(([]byte)(text))
 
 }
 
-func streamTweet(api *anaconda.TwitterApi,ids map[string]string) {
+func streamTweet(api *anaconda.TwitterApi, username, idstr string,ch chan string){
 
-	for username := range ids{
-		v := url.Values{}
-		v.Set("follow", ids[username])
+	v := url.Values{}
+	v.Set("follow", idstr)
 
-		stream := api.PublicStreamFilter(v)
+	stream := api.PublicStreamFilter(v)
 
-		fmt.Println("stream started: @" + username)
+	fmt.Println("started stream: @" + username)
 
-		go func(){
-			for {
-				x := <-stream.C
-				switch tweet := x.(type) {
-				case anaconda.Tweet:
-					rep := regexp.MustCompile(`^@.*\s`)
-					if !rep.MatchString(tweet.Text) { // @???で誰かに向けたツイート以外を取得
-						fileExport(username+"/"+tweet.IdStr, tweet.Text+"\n")
-						fmt.Println(tweet.Text)
-						fmt.Println("--------")
-					}
-				default:
-					fmt.Println("unkown type(%T): %v \n", x, x)
-				}
+	for {
+		x := <-stream.C
+		switch tweet := x.(type) {
+		case anaconda.Tweet:
+			rep := regexp.MustCompile(`^@.*\s`)
+			if !rep.MatchString(tweet.Text) { // @???で誰かに向けたツイート以外を取得
+				fileExport(username + "/" + idstr,tweet.Text)
+				fmt.Println(tweet.Text + "from @" + username)
+				fmt.Println("--------")
 			}
-		}()
-
+		default:
+			fmt.Println("unkown type(%T): %v \n", x, x)
+			ch <- "end"
+		}
 	}
-
 }
 
 func getTwitterApi() *anaconda.TwitterApi {
